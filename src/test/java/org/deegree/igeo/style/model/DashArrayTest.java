ddfff03ce345400b64c612cc614c13eeac5574b9
Jeronimo Wanhoff <wanhoff@lat-lon.de>
2012-09-12 10:12:31 +0000

    Rearrangement of tests.

--- src/test/java/org/deegree/igeo/style/model/DashArrayTest.java
+++ src/test/java/org/deegree/igeo/style/model/DashArrayTest.java
@@ -53,138 +53,79 @@ public class DashArrayTest {
 
 	@Test
 	public void test_equals_with_same_DashArray() {
-		//arrange
+		// arrange
 		String daName = "testDashArray";
-		float[] daArray = {1f, 2f};
+		float[] daArray = { 1f, 2f };
 		DashArray da = new DashArray(daName, daArray);
-		//act
-		//assert
-		assertTrue (da.equals(da));
-	}
-	
-	@Test
-	public void test_equals_with_same_values () {
-		//arrange
-		String daName = "testDashArray";
-		float[] daArray = {1f, 2f};
-		DashArray da1 = new DashArray(daName, daArray.clone());
-		DashArray da2 = new DashArray(daName, daArray.clone());
-		//act
-		//assert
-		assertTrue (da1.equals(da2));
-	}
-
-	@Test
-	public void test_equals_with_same_name_and_diffenent_arrays () {
-		//arrange
-		String daName = "testDashArray";
-		float[] daArray1 = {1f, 2f};
-		float[] daArray2 = {2f, 3f};
-		DashArray da1 = new DashArray(daName, daArray1);
-		DashArray da2 = new DashArray(daName, daArray2);
-		//act
-		//assert
-		assertFalse(da1.equals(da2));
-	}
-
-	@Test
-	public void test_equals_with_different_name_and_same_array () {
-		//arrange
-		String daName1 = "testDashArray1";
-		String daName2 = "testDashArray2";
-		float[] daArray = {1f, 2f};
-		DashArray da1 = new DashArray(daName1, daArray);
-		DashArray da2 = new DashArray(daName2, daArray);
-		//act
-		//assert
-		assertFalse(da1.equals(da2));
-	}
-
-	@Test
-	public void test_equals_with_different_values () {
-		//arrange
-		String daName1 = "testDashArray1";
-		String daName2 = "testDashArray2";
-		float[] daArray1 = {1f, 2f};
-		float[] daArray2 = {1f, 2f};
-		DashArray da1 = new DashArray(daName1, daArray1);
-		DashArray da2 = new DashArray(daName2, daArray2);
-		//act
-		//assert
-		assertFalse(da1.equals(da2));
-	}
-	
-	@Test
-	public void test_hashCode_with_same_DashArray() {
-		//arrange
-		String daName = "testDashArray";
-		float[] daArray = {1f, 2f};
-		DashArray da = new DashArray(daName, daArray);
-		//act
+		// act
 		int hashCode1 = da.hashCode();
 		int hashCode2 = da.hashCode();
-		//assert
-		assertTrue (hashCode1==hashCode2);
+		// assert
+		assertTrue(da.equals(da));
+		assertTrue(hashCode1==hashCode2);
 	}
 
 	@Test
-	public void test_hashCode_with_same_values() {
-		//arrange
+	public void test_equals_with_same_values() {
+		// arrange
 		String daName = "testDashArray";
-		float[] daArray = {1f, 2f};
+		float[] daArray = { 1f, 2f };
 		DashArray da1 = new DashArray(daName, daArray.clone());
 		DashArray da2 = new DashArray(daName, daArray.clone());
-		//act
+		// act
 		int hashCode1 = da1.hashCode();
 		int hashCode2 = da2.hashCode();
-		//assert
-		assertTrue (hashCode1==hashCode2);
+		// assert
+		assertTrue(da1.equals(da2));
+		assertTrue(hashCode1 == hashCode2);
 	}
 
 	@Test
-	public void test_hashCode_with_same_name_and_different_array() {
-		//arrange
+	public void test_equals_with_same_name_and_diffenent_arrays() {
+		// arrange
 		String daName = "testDashArray";
-		float[] daArray1 = {1f, 2f};
-		float[] daArray2 = {2f, 3f};
+		float[] daArray1 = { 1f, 2f };
+		float[] daArray2 = { 2f, 3f };
 		DashArray da1 = new DashArray(daName, daArray1);
 		DashArray da2 = new DashArray(daName, daArray2);
-		//act
+		// act
 		int hashCode1 = da1.hashCode();
 		int hashCode2 = da2.hashCode();
-		//assert
-		assertFalse (hashCode1==hashCode2);
+		// assert
+		assertFalse(da1.equals(da2));
+		assertFalse(hashCode1==hashCode2);
 	}
 
 	@Test
-	public void test_hashCode_with_different_name_and_same_array() {
-		//arrange
+	public void test_equals_with_different_name_and_same_array() {
+		// arrange
 		String daName1 = "testDashArray1";
-		String daName2 = "testDashArray2";		
-		float[] daArray = {1f, 2f};
-		DashArray da1 = new DashArray(daName1, daArray.clone());
-		DashArray da2 = new DashArray(daName2, daArray.clone());
-		//act
+		String daName2 = "testDashArray2";
+		float[] daArray = { 1f, 2f };
+		DashArray da1 = new DashArray(daName1, daArray);
+		DashArray da2 = new DashArray(daName2, daArray);
+		// act
 		int hashCode1 = da1.hashCode();
 		int hashCode2 = da2.hashCode();
-		//assert
-		assertFalse (hashCode1==hashCode2);
+		// assert
+		assertFalse(da1.equals(da2));
+		assertFalse(hashCode1==hashCode2);
 	}
 
 	@Test
-	public void test_hashCode_with_different_values() {
-		//arrange
+	public void test_equals_with_different_values() {
+		// arrange
 		String daName1 = "testDashArray1";
-		String daName2 = "testDashArray2";		
-		float[] daArray1 = {1f, 2f};
-		float[] daArray2 = {2f, 3f};
+		String daName2 = "testDashArray2";
+		float[] daArray1 = { 1f, 2f };
+		float[] daArray2 = { 1f, 2f };
 		DashArray da1 = new DashArray(daName1, daArray1);
 		DashArray da2 = new DashArray(daName2, daArray2);
-		//act
+		// act
 		int hashCode1 = da1.hashCode();
 		int hashCode2 = da2.hashCode();
-		//assert
-		assertFalse (hashCode1==hashCode2);
+		// assert
+		assertFalse(da1.equals(da2));
+		assertFalse(hashCode1==hashCode2);
 	}
-
 }
