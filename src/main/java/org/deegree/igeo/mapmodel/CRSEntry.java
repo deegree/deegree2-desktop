4b3ce76f50e0295ddc1e6036c4fe8bfa1635f0ed
Jeronimo Wanhoff <wanhoff@lat-lon.de>
2012-10-11 17:21:52 +0000

    Added hashCode and replaced equals method

--- src/main/java/org/deegree/igeo/mapmodel/CRSEntry.java
+++ src/main/java/org/deegree/igeo/mapmodel/CRSEntry.java
@@ -75,12 +75,31 @@ public class CRSEntry implements Comparable<CRSEntry> {
         return name;
     }
 
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( code == null ) ? 0 : code.hashCode() );
+        return result;
+    }
+
     @Override
     public boolean equals( Object obj ) {
-        if ( obj == null || !( obj instanceof CRSEntry ) ) {
+        if ( this == obj )
+            return true;
+        if ( obj == null )
             return false;
-        }
-        return code.equalsIgnoreCase( ( (CRSEntry) obj ).getCode() );
+        if ( getClass() != obj.getClass() )
+            return false;
+        CRSEntry other = (CRSEntry) obj;
+        if ( code == null ) {
+            if ( other.code != null )
+                return false;
+        } else if ( !code.equalsIgnoreCase( other.code ) )
+            return false;
+        return true;
     }
 
     @Override
