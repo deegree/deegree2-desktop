b7850ce8a0025b4c421019ebdf9f9b533dc8fa2e
Jeronimo Wanhoff <wanhoff@lat-lon.de>
2012-10-11 17:40:08 +0000

    bugfix: changed float equality test to equality test within a range of 0.0000001

--- src/main/java/org/deegree/igeo/style/model/LinearGradient.java
+++ src/main/java/org/deegree/igeo/style/model/LinearGradient.java
@@ -1,6 +1,6 @@
 /*----------------    FILE HEADER  ------------------------------------------
  This file is part of deegree.
- Copyright (C) 2001-2007 by:
+ Copyright (C) 2001-2012 by:
  Department of Geography, University of Bonn
  http://www.giub.uni-bonn.de/deegree/
  lat/lon GmbH
@@ -19,12 +19,11 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  Contact:
 
- Andreas Poth
  lat/lon GmbH
  Aennchenstr. 19
  53177 Bonn
  Germany
- E-Mail: poth@lat-lon.de
+ E-Mail: info@lat-lon.de
 
  Prof. Dr. Klaus Greve
  Department of Geography
@@ -51,6 +50,7 @@ import org.deegree.igeo.settings.ColorListEntry;
 /**
  * <code>FillGradient</code>
  * 
+ * @author <a href="mailto:wanhoff@lat-lon.de">Jeronimo Wanhoff</a>
  * @author <a href="mailto:buesching@lat-lon.de">Lyn Buesching</a>
  * @author last edited by: $Author$
  * 
@@ -165,7 +165,7 @@ public class LinearGradient implements Fill {
             boolean isFillPattern = graphicSymbol != null && graphicSymbol instanceof FillPattern;
 
             for ( int i = 0; i < count; i++ ) {
-                if ( position == cf.fraction ) {
+                if ( Math.abs (position - cf.fraction) < 0.0000001 ) {
                     if ( isFillPattern ) {
                         FillPattern newFP = new FillPattern( (FillPattern) graphicSymbol );
                         newFP.setColor( cf.color );
