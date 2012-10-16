0e4d2bd6e695c75ef8914f8219347df28b5d19c3
Jeronimo Wanhoff <wanhoff@lat-lon.de>
2012-10-16 10:48:29 +0000

    Added default case to switch statement

--- src/main/java/org/deegree/igeo/style/perform/LineVisualPropertyPerformer.java
+++ src/main/java/org/deegree/igeo/style/perform/LineVisualPropertyPerformer.java
@@ -1,6 +1,6 @@
 /*----------------    FILE HEADER  ------------------------------------------
  This file is part of deegree.
- Copyright (C) 2001-2007 by:
+ Copyright (C) 2001-2012 by:
  Department of Geography, University of Bonn
  http://www.giub.uni-bonn.de/deegree/
  lat/lon GmbH
@@ -19,12 +19,11 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  Contact:
 
- Andreas Poth
  lat/lon GmbH
  Aennchenstr. 19
  53177 Bonn
  Germany
- E-Mail: poth@lat-lon.de
+ E-Mail: info@lat-lon.de
 
  Prof. Dr. Klaus Greve
  Department of Geography
@@ -57,6 +56,7 @@ import org.deegree.model.filterencoding.PropertyName;
 /**
  * <code>LineVisualPropertyPerformer</code>
  * 
+ * @author <a href="mailto:wanhoff@lat-lon.de">Jeronimo Wanhoff</a>
  * @author <a href="mailto:buesching@lat-lon.de">Lyn Buesching</a>
  * @author last edited by: $Author$
  * 
@@ -166,8 +166,9 @@ public class LineVisualPropertyPerformer extends AbstractVisualPropertyPerformer
                 this.dashArray = ( (DashArray) value ).getDashArray();
             }
             break;
+		default:
+			break;
         }
-
     }
 
     @Override
