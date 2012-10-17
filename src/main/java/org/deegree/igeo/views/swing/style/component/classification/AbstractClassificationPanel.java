164b2ba3fc54697276aa6663c5f4c6678ed6e768
Jeronimo Wanhoff <wanhoff@lat-lon.de>
2012-10-17 15:53:12 +0000

    Commented code (currently) without use.

--- src/main/java/org/deegree/igeo/views/swing/style/component/classification/AbstractClassificationPanel.java
+++ src/main/java/org/deegree/igeo/views/swing/style/component/classification/AbstractClassificationPanel.java
@@ -1,6 +1,6 @@
 /*----------------    FILE HEADER  ------------------------------------------
  This file is part of deegree.
- Copyright (C) 2001-2007 by:
+ Copyright (C) 2001-2012 by:
  Department of Geography, University of Bonn
  http://www.giub.uni-bonn.de/deegree/
  lat/lon GmbH
@@ -19,12 +19,11 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  Contact:
 
- Andreas Poth
  lat/lon GmbH
  Aennchenstr. 19
  53177 Bonn
  Germany
- E-Mail: poth@lat-lon.de
+ E-Mail: info@lat-lon.de
 
  Prof. Dr. Klaus Greve
  Department of Geography
@@ -151,8 +150,9 @@ import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
 /**
- * <code>AbstractClassificationPanel</code>
+ * <code>AbstractClassificationPanel</code> TODO add class documentation
  * 
+ * @author <a href="mailto:wanhoff@lat-lon.de">Jeronimo Wanhoff</a>
  * @author <a href="mailto:buesching@lat-lon.de">Lyn Buesching</a>
  * @author last edited by: $Author$
  * 
@@ -385,10 +385,10 @@ public abstract class AbstractClassificationPanel extends JPanel implements Acti
     private <V extends Comparable<V>> ThematicGrouping<V> getGrouping( ThematicGroupingInformation<V> thematicGrouping ) {
         GROUPINGTYPE type = thematicGrouping.getType();
         // TODO: ask user for classification
-        if ( !thematicGrouping.getType().equals( UNIQUE ) && !thematicGrouping.getType().equals( QUANTILE )
-             && !thematicGrouping.getType().equals( EQUAL ) && !thematicGrouping.getType().equals( QUALITY ) ) {
-            // type = askUserForClassificationType();
-        }
+//        if ( !thematicGrouping.getType().equals( UNIQUE ) && !thematicGrouping.getType().equals( QUANTILE )
+//             && !thematicGrouping.getType().equals( EQUAL ) && !thematicGrouping.getType().equals( QUALITY ) ) {
+//             type = askUserForClassificationType();
+//        }
         ThematicGrouping<V> grouping;
         switch ( type ) {
         case UNIQUE:
