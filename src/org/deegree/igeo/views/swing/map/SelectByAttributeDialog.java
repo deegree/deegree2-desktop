bb2c9a32818693a8d7cdcc21a0cbb2acff3274ad
Alexander Erben <erben@lat-lon.de>
2012-12-04 08:13:37 +0000

    Implement GML-ID search in SelectByAttributeDialog - rudimentary GUI-integration

--- src/org/deegree/igeo/views/swing/map/SelectByAttributeDialog.java
+++ src/org/deegree/igeo/views/swing/map/SelectByAttributeDialog.java
@@ -36,17 +36,21 @@
 package org.deegree.igeo.views.swing.map;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 
 import org.deegree.datatypes.QualifiedName;
@@ -135,80 +139,117 @@ public class SelectByAttributeDialog extends JDialog {
         setModal( true );
         setVisible( true );
     }
+    
+    private void performFilterSearch(List<Operation> attributeOperations, String attributeOperation) {
+		List<Operation> allOperations = new ArrayList<Operation>();
+        for ( Operation operation : attributeOperations ) {
+            allOperations.add( operation );
+        }
+        if ( allOperations.size() > 0 ) {
+            Operation finalOperation = null;
+            if ( allOperations.size() == 1 ) {
+                // if only one operation is created, it is not needed to create a logical operation
+                finalOperation = allOperations.get( 0 );
+            } else {
+                // create a logical operation out of all operations combined with the logical
+                // operation
+                int operation = OperationDefines.getIdByName( attributeOperation );
+                finalOperation = new LogicalOperation( operation, allOperations );
+            }
+            
+            // create the attribute filter                 
+            Filter filter = new ComplexFilter( finalOperation );
+            Command cmd = new SelectFeatureCommand( layer, filter, false );
+            try {
+            	// apply the attribute filter
+                appCont.getCommandProcessor().executeSychronously( cmd, true );
+            } catch ( Exception ex ) {
+                DialogFactory.openErrorDialog( appCont.getViewPlatform(), SelectByAttributeDialog.this,
+                                               Messages.getMessage( getLocale(), "$MD11600" ),
+                                               Messages.getMessage( getLocale(), "$MD11601" ), ex );
+            }
+        } else {
+            DialogFactory.openWarningDialog( appCont.getViewPlatform(), this,
+                                             Messages.getMessage( getLocale(), "$MD11602" ),
+                                             Messages.getMessage( getLocale(), "$MD11603" ) );
+            return;
+        }
+    }
+    
+    private void performFidSearch( List<Identifier> fids ) {
+    	System.out.println("Searching for "+fids.get(0)+" with URI "+fids.get(0).toString());
+    	
+    	if ( fids.size() > 0 ) {
+            Command cmdFid = new SelectFeatureCommand( layer, fids, false);
+            try { 
+            	appCont.getCommandProcessor().executeSychronously( cmdFid, true );
+            } catch ( Exception ex ) {
+                DialogFactory.openErrorDialog( appCont.getViewPlatform(), SelectByAttributeDialog.this,
+                                               Messages.getMessage( getLocale(), "$MD11600" ),
+                                               Messages.getMessage( getLocale(), "$MD11601" ), ex );
+            }
+        } else {
+            DialogFactory.openWarningDialog( appCont.getViewPlatform(), this,
+                                             Messages.getMessage( getLocale(), "$MD11602" ),
+                                             Messages.getMessage( getLocale(), "$MD11603" ) );
+            return;
+        }
+    }
 
     private void initGUI( List<QualifiedName> propertyNames, QualifiedName featureType,
                           DictionaryCollection dictCollection ) {
+    	
         setLayout( new BorderLayout() );
         
-        // adds an attribute panel that holds the criteria to be applied in the feature search
-        add( attributePanel = new AttributeCriteriaPanel( propertyNames, featureType, dictCollection ), BorderLayout.CENTER );
+        final JPanel attributePanel = new JPanel(new BorderLayout() );
+        final AttributeCriteriaPanel attributeCriteriaPanel = new AttributeCriteriaPanel( propertyNames, featureType, dictCollection );
+        attributePanel.add(attributeCriteriaPanel, BorderLayout.CENTER );
+
 
         JPanel applyAndCloseButtonsPanel = new JPanel( new FlowLayout( FlowLayout.LEFT ) );
-        
-        /** adds an gml-id text field
-        final JTextField gmlIdValueTextField = new JTextField("Hier gml-id eingeben",120);
-        applyAndCloseButtonsPanel.add(gmlIdValueTextField); **/
-        
         JButton applyButton = new JButton( Messages.getMessage( getLocale(), "$MD11598" ), IconRegistry.getIcon( "accept.png" ) );
         applyButton.addActionListener( new ActionListener() {
-        	
         	// applies the filter operations given in the attribute panel
             public void actionPerformed( ActionEvent e ) {               
-            	List<Operation> allOperations = new ArrayList<Operation>();
-                List<Operation> attOperations = attributePanel.getOperations();
-                for ( Operation operation : attOperations ) {
-                    allOperations.add( operation );
-                }
-                if ( allOperations.size() > 0 ) {
-                    Operation finalOperation = null;
-                    if ( allOperations.size() == 1 ) {
-                        // if only one operation is created, it is not needed to create a logical operation
-                        finalOperation = allOperations.get( 0 );
-                    } else {
-                        // create a logical operation out of all operations combined with the logical
-                        // operation
-                        int operation = OperationDefines.getIdByName( attributePanel.getOperation() );
-                        finalOperation = new LogicalOperation( operation, allOperations );
-                    }
-                    
-                    /** Enables GML-ID search
-                    Identifier identifier = new Identifier(gmlIdValueTextField.getText(), null);
-                    List<Identifier> fid = new ArrayList<Identifier>();
-                    fid.add(identifier); 
-                    Command cmdFid = new SelectFeatureCommand( layer, fid, false);
-                    **/                   
-                    
-                    // create the attribute filter                 
-                    Filter filter = new ComplexFilter( finalOperation );
-                    Command cmd = new SelectFeatureCommand( layer, filter, false );
-                    try {
-                    	// apply the attribute filter
-                        appCont.getCommandProcessor().executeSychronously( cmd, true );
-                        // appCont.getCommandProcessor().executeSychronously( cmdFid, true );
-                    } catch ( Exception ex ) {
-                        DialogFactory.openErrorDialog( appCont.getViewPlatform(), SelectByAttributeDialog.this,
-                                                       Messages.getMessage( getLocale(), "$MD11600" ),
-                                                       Messages.getMessage( getLocale(), "$MD11601" ), ex );
-                    }
-                } else {
-                    DialogFactory.openWarningDialog( appCont.getViewPlatform(), this,
-                                                     Messages.getMessage( getLocale(), "$MD11602" ),
-                                                     Messages.getMessage( getLocale(), "$MD11603" ) );
-                    return;
-                }
-            }
-        } );
+            	performFilterSearch(attributeCriteriaPanel.getOperations(), attributeCriteriaPanel.getOperation());
+            }			
+        } );      
         applyAndCloseButtonsPanel.add( applyButton );
+        // adds an attribute panel that holds the criteria to be applied in the feature search
+
+        /**
+        final JTextField gmlIdValueTextField = new JTextField("Hier gml-id eingeben",120);
+        attributePanel.add(gmlIdValueTextField, BorderLayout.NORTH);
+        JButton gmlSearchButton = new JButton( "GML-ID search" , IconRegistry.getIcon( "accept.png" ) );
+        
+        gmlSearchButton.addActionListener( new ActionListener() {
+        	// applies the filter operations given in the attribute panel
+            public void actionPerformed( ActionEvent e ) {
+            	Identifier gmlId = new Identifier (gmlIdValueTextField.getText(), null);
+            	List<Identifier> gmlIdList = new ArrayList<Identifier>();
+            	gmlIdList.add(gmlId);
+            	performFidSearch( gmlIdList );
+            }			
+        } );  
+        
+       
+        applyAndCloseButtonsPanel.add(gmlSearchButton); **/
+        
         JButton closeButton = new JButton( Messages.getMessage( getLocale(), "$MD11599" ),
                                        IconRegistry.getIcon( "cancel.png" ) );
+        
         closeButton.addActionListener( new ActionListener() {
-
             public void actionPerformed( ActionEvent e ) {
                 dispose();
             }
         } );
         applyAndCloseButtonsPanel.add( closeButton );
-        add( applyAndCloseButtonsPanel, BorderLayout.SOUTH );
+        
+        attributePanel.add(applyAndCloseButtonsPanel, BorderLayout.SOUTH );
+        
+        JTabbedPane jTabbedPane1 = new javax.swing.JTabbedPane();  
+        jTabbedPane1.addTab("Filter", attributePanel);
+        add (jTabbedPane1);
     }
 
 }
