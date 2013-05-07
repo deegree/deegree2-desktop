9c2eb7b8c15eeeb88340131667f9ed4374b3f294
lgoltz <goltz@lat-lon.de>
2013-05-07 15:43:23 +0200

    added commit

--- src/org/deegree/igeo/views/swing/map/CopyCoordinatesPanel.java
+++ src/org/deegree/igeo/views/swing/map/CopyCoordinatesPanel.java
@@ -1,3 +1,39 @@
+//$HeadURL$
+/*----------------    FILE HEADER  ------------------------------------------
+ This file is part of deegree.
+ Copyright (C) 2001-2013 by:
+ Department of Geography, University of Bonn
+ http://www.giub.uni-bonn.de/deegree/
+ lat/lon GmbH
+ http://www.lat-lon.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ Contact:
+
+ lat/lon GmbH
+ Aennchenstr. 19
+ 53177 Bonn
+ Germany
+ http://www.lat-lon.de
+
+ Prof. Dr. Klaus Greve
+ Department of Geography
+ University of Bonn
+ Meckenheimer Allee 166
+ 53115 Bonn
+ Germany
+ E-Mail: greve@giub.uni-bonn.de
+ ---------------------------------------------------------------------------*/
 package org.deegree.igeo.views.swing.map;
 
 import java.awt.Component;
@@ -13,8 +49,17 @@ import javax.swing.JPanel;
 import org.deegree.framework.log.ILogger;
 import org.deegree.framework.log.LoggerFactory;
 import org.deegree.igeo.i18n.Messages;
+import org.deegree.igeo.modules.CopyCoordinatesModule;
 import org.deegree.igeo.state.mapstate.MapTool;
 
+/**
+ * Handles MouseEvents for the {@link CopyCoordinatesModule}
+ * 
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz</a>
+ * @author last edited by: $Author: lyn $
+ * 
+ * @version $Revision: $, $Date: $
+ */
 public class CopyCoordinatesPanel extends JPanel implements MouseListener {
 
     private static final long serialVersionUID = -6526681726375085230L;
